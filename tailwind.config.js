a1477b95d28771559b3cee3aed92d4bafe33dcf0
Sharon Amelinda <sharonamelindaa@gmail.com>
2024-10-28 10:50:04 +0700

    Modules installation
    - Tailwind CSS
    - Color
    - CSS Setting
    - Alpine JS CDN

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'weserve-purple' : "#640EF1",
+        'weserve-navy' : '#080C2E',
+        'weserve-grey' : '#5D5D7C',
+        'weserve-yellow' : '#FFD15A',
+        'weserve-light' : '#8D90A8'
+      },
+      fontFamily: {
+        'clash-display': 'Clash Display, sans-serif'
+      }
+    },
+  },
+  plugins: [],
+}
+
